b1d155152e5151514d7ce368fd2e99c644957e09
Aut-172 <1721892105@qq.com>
2026-06-04 16:16:27 +0800

    init

--- parking-service/src/main/java/com/demo/parkingservice/mapper/ParkingLotMapper.java
+++
@@ -1,9 +0,0 @@
-package com.demo.parkingservice.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.demo.common.entity.ParkingLot;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface ParkingLotMapper extends BaseMapper<ParkingLot> {
-}
